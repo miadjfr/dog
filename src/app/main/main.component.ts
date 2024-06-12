9804e368a340a297142a3859ab663390dedb41ba
Miad jfr <miad,jafari@live.com>
2024-06-12 17:48:54 +0330

    4

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component ,OnInit
+ } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+    constructor() { }
 
-    
+    ngOnInit() {
+      
+    }
+
+
+
 
 }
